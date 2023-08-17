211727a615fb4c1b3dc040102fd2bfd457633161
lagzian <134182864+lagzian@users.noreply.github.com>
2023-08-17 19:08:51 +0330

    Update output.py

--- utils/speedtest/output.py
+++ utils/speedtest/output.py
@@ -20,7 +20,6 @@ def read_json(file):
     with open(file, 'r', encoding='utf-8') as f:
         print('Reading out.json')
         proxies_all = json.load(f)["nodes"]
-        f.close()
     return proxies_all
 
 def output(list, num):
@@ -35,7 +34,6 @@ def output(list, num):
 
     with open('./LogInfo.txt', 'w') as f1:
         f1.writelines(output_list)
-        f1.close()
         print('Write Log Success!')
 
     output_list = [item["link"] for item in list]
@@ -52,71 +50,41 @@ def output(list, num):
     ss_outputs = []
 
     for output in output_list:
-        if str(output).startswith("vmess://"):
-            vmess_outputs.append(output)
-        elif str(output).startswith("vless://"):
-            vless_outputs.append(output)
-        elif str(output).startswith("trojan://"):
-            trojan_outputs.append(output)
-        elif str(output).startswith("ssr://"):
-            ssr_outputs.append(output)
-        elif str(output).startswith("ss://"):
-            ss_outputs.append(output)
-
-    with open(os.path.join(splitted_output, "vmess.txt"), 'w') as f:
-        vmess_content = "\n".join(vmess_outputs)
-        f.write(vmess_content)
-        print('Write vmess splitted Success!')
-        f.close()
-
-    with open(os.path.join(splitted_output, "vless.txt"), 'w') as f:
-        vless_content = "\n".join(vless_outputs)
-        f.write(vless_content)
-        print('Write vless splitted Success!')
-        f.close()
+        if not str(output).startswith(("vmess://", "vless://", "ssr://")):
+            if str(output).startswith("trojan://"):
+                trojan_outputs.append(output)
+            elif str(output).startswith("ss://"):
+                ss_outputs.append(output)
 
     with open(os.path.join(splitted_output, "trojan.txt"), 'w') as f:
         trojan_content = "\n".join(trojan_outputs)
         f.write(trojan_content)
         print('Write trojan splitted Success!')
-        f.close()
-
-    with open(os.path.join(splitted_output, "ssr.txt"), 'w') as f:
-        ssr_content = "\n".join(ssr_outputs)
-        f.write(ssr_content)
-        print('Write ssr splitted Success!')
-        f.close()
 
     with open(os.path.join(splitted_output, "ss.txt"), 'w') as f:
         ss_content = "\n".join(ss_outputs)
         f.write(ss_content)
         print('Write ss splitted Success!')
-        f.close()
 
     with open(sub_all_base64, 'w+', encoding='utf-8') as f:
         f.write(content_base64)
         print('Write All Base64 Success!')
-        f.close()
 
     with open(Trinity_file_base64, 'w+', encoding='utf-8') as f:
         f.write(content_base64_part)
         print('Write Part Base64 Success!')
-        f.close()
 
     with open(sub_all, 'w') as f:
         f.write(content)
         print('Write All Success!')
-        f.close()
 
     with open(Trinity_Base, 'w') as f:
         f.write(content)
         print('Write Base Success!')
-        f.close()
 
     with open(Trinity_file, 'w') as f:
         f.write('\n'.join(output_list[0:num]))
         print('Write Part Base Success!')
-        f.close()
 
     return content
 
