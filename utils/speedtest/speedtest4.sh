0b2867cfeeb0eb72125582286642466b0f55604c
lagzian <134182864+lagzian@users.noreply.github.com>
2024-12-17 18:29:24 +0330

    Update speedtest4.sh

--- utils/speedtest/speedtest4.sh
+++ utils/speedtest/speedtest4.sh
@@ -1,5 +1,5 @@
 #准备好所需文件
-wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.14.1/lite-linux-amd64-v0.14.1.gz
+wget -O lite-linux-amd64.gz https://github.com/xxf098/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64-v0.15.0.gz
 gzip -d lite-linux-amd64.gz
 wget -O lite_config2.json https://raw.githubusercontent.com/lagzian/SS-Collector/main/utils/speedtest/lite_config3.json
 #运行 LiteSpeedTest
